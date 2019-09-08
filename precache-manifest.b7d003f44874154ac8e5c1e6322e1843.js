6a07ebe047a884cd866a21c2d1cc31c3c01c921c
Kh-Ol-An <olegkhristenko@gmail.com>
2019-09-08 15:21:42 +0300

    Updates

---
+++ precache-manifest.b7d003f44874154ac8e5c1e6322e1843.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56385b86916c18d481dc73d405ea771c",
+    "url": "/goit-react-hw-04-pet-store/index.html"
+  },
+  {
+    "revision": "6edde518eab7a6fbd104",
+    "url": "/goit-react-hw-04-pet-store/static/css/main.99bf6b95.chunk.css"
+  },
+  {
+    "revision": "a1a0e17d5e9bfe61645a",
+    "url": "/goit-react-hw-04-pet-store/static/js/2.a9042089.chunk.js"
+  },
+  {
+    "revision": "6edde518eab7a6fbd104",
+    "url": "/goit-react-hw-04-pet-store/static/js/main.d0342813.chunk.js"
+  },
+  {
+    "revision": "a9af00c92ff49c0cfd03",
+    "url": "/goit-react-hw-04-pet-store/static/js/runtime~main.cfafcf1d.js"
+  }
+]);
